3708a33b7910c4fd2940fd9a830bc05e9a2e4d9b
Alex Puiu <alexpuiu20@yahoo.com>
2019-02-09 18:50:38 +0200

    Create the side-navigation bar, style it and add JavaScript functions to it.

---
+++ src/_component/side-navigation/side-navigation.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("side-nav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0px";
+}
+
+function openNav() {
+    document.getElementById("side-nav").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
