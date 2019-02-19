e1c528a000a1bf75a5088576c4a4d4c7b0fb146d
a11exandru <alexpuiu20@yahoo.com>
2019-02-19 23:35:39 +0200

    Change the layout for the last version.

--- src/_component/side-navigation/side-navigation.js
+++ src/_component/side-navigation/side-navigation.js
@@ -5,13 +5,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("side-nav").style.width = "0px";
 }
-
-function openNav() {
-    document.getElementById("side-nav").style.width = "220px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
-}
-
-function closeNav() {
-    document.getElementById("side-nav").style.width = "0";
-    document.body.style.backgroundColor = "white";
-}
